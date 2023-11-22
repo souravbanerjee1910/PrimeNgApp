56195c41eb2977d213cba9d6d2f40c18e057848b
Sourav <sourav.banerjee@facileserv.com>
2023-11-22 19:52:31 +0530

    LandingPageUI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'PrimeNgApp';
+
+  foodcardData = [
+    {
+      src: '../assets/logo/rest_img/albert-YYZU0Lo1uXE-unsplash.jpg',
+      name: 'Indian cuisine'
+    },
+    {
+      src: '../assets/logo/rest_img/alessio-dandi-LusjIyLfZ70-unsplash.jpg',
+      name: 'Italian cuisine'
+    },
+    {
+      src: '../assets/logo/rest_img/jason-leung-poI7DelFiVA-unsplash.jpg',
+      name: 'Chinese cuisine'
+    },
+    {
+      src: '../assets/logo/rest_img/kayleigh-harrington-yhn4okt6ci0-unsplash.jpg',
+      name: 'Thai cuisine'
+    },
+    {
+      src: '../assets/logo/rest_img/patrick-tomasso-GXXYkSwndP4-unsplash.jpg',
+      name: 'Soul food'
+    },
+    {
+      src: '../assets/logo/rest_img/ruben-ramirez-xhKG01FN2uk-unsplash.jpg',
+      name: 'Japanese Cuisine'
+    }
+  ]
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AccordionModule } from 'primeng/accordion'; // Use 'primeng/accordion' for version 12.2.3
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    AccordionModule,
+    FormsModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
