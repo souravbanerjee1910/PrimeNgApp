99a39a639c9b03a87178a90632322b134ba837a3
Sourav <sourav.banerjee@facileserv.com>
2023-11-28 17:34:46 +0530

    implementation of Angular Property Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,35 +3,119 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  logo = 'D!';
+  title = 'Search Places Near to you';
+  Search='Quick Search';
+  quickSearchSubTitle='Find Resturants By MealType';
 
+  // food card data
   foodcardData = [
     {
       src: '../assets/logo/rest_img/albert-YYZU0Lo1uXE-unsplash.jpg',
-      name: 'Indian cuisine'
+      name: 'Indian cuisine',
     },
     {
       src: '../assets/logo/rest_img/alessio-dandi-LusjIyLfZ70-unsplash.jpg',
-      name: 'Italian cuisine'
+      name: 'Italian cuisine',
     },
     {
       src: '../assets/logo/rest_img/jason-leung-poI7DelFiVA-unsplash.jpg',
-      name: 'Chinese cuisine'
+      name: 'Chinese cuisine',
     },
     {
       src: '../assets/logo/rest_img/kayleigh-harrington-yhn4okt6ci0-unsplash.jpg',
-      name: 'Thai cuisine'
+      name: 'Thai cuisine',
     },
     {
       src: '../assets/logo/rest_img/patrick-tomasso-GXXYkSwndP4-unsplash.jpg',
-      name: 'Soul food'
+      name: 'Soul food',
     },
     {
       src: '../assets/logo/rest_img/ruben-ramirez-xhKG01FN2uk-unsplash.jpg',
-      name: 'Japanese Cuisine'
-    }
-  ]
+      name: 'Japanese Cuisine',
+    },
+  ];
 
+  // Search Location Data
+
+  searchLocationData = [
+    {
+      city: 'Chandrapura',
+      state: 'Jharkhand',
+    },
+    {
+      city: 'Chatra',
+      state: 'Jharkhand',
+    },
+
+    {
+      city: 'Daltonganj',
+      state: 'Jharkhand',
+    },
+    {
+      city: 'Deoghar',
+      state: 'Jharkhand',
+    },
+    {
+      city: 'Dhanbad',
+      state: 'Jharkhand',
+    },
+    {
+      city: 'Dumka',
+      state: 'Jharkhand',
+    },
+  ];
+
+  // Resturant Names
+  resturantNameData = [
+    {
+      restName: 'Hydrabadi Zaika',
+    },
+    {
+      restName: 'Behrauz Biryani',
+    },
+    {
+      restName: 'Reddys Biryani',
+    },
+    {
+      restName: 'Almor',
+    },
+    {
+      restName: 'Moms Biryani',
+    },
+    {
+      restName: 'Paradise Biryani',
+    },
+    {
+      restName: 'Nandus Biryani',
+    },
+  ];
+
+  // Dishes Available
+  dishNameData = [
+    {
+      MenuItem: 'Chicken Tandoor Dum Biryani',
+    },
+    {
+      MenuItem: 'Hydrabadi Dum Biryani',
+    },
+    {
+      MenuItem: 'Chicken Tandoor Masala Dum Biryani',
+    },
+    {
+      MenuItem: 'Veg Dum Biryani',
+    },
+    {
+      MenuItem: 'Maska Dum Biryani',
+    },
+    {
+      MenuItem: 'Chicken Tandoor Kabab',
+    },
+    {
+      MenuItem: 'Veg Kabab',
+    },
+  ];
 }
--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent {
-
+  copy = 'Developer Funnel';
+  contact = 'Contact';
+  about = 'About';
 }
